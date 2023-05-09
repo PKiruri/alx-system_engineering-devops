2367fa43e2e3595908ecb7b2b322e72f0ee2614a
PKiruri <107044946+PKiruri@users.noreply.github.com>
2023-05-09 13:48:14 +0300

    0

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -76,3 +76,8 @@ def count_words(subreddit, word_list):
 
 if __name__ == "__main__":
     count_words(argv[1], argv[2].split(" "))
+    if len(sys.argv) < 3:
+        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
+        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+    else:
+        result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
